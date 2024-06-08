2d9f368b8084d1e27980e705094d2cc83bd172af
eramos9070 <elias.ramos@unah.hn>
2024-06-08 16:45:46 -0600

    Create Ejercicio2.dart

---
+++ Ejercicio2.dart
@@ -0,0 +1,28 @@
+
+void main() {
+
+final postres = [
+    'cheescake',
+    'pie de limon',
+    'donas glaseadas',
+    'chocolates',
+    'flan',
+    'galletas con chispas de cocolate'
+  ];
+
+
+  arreglo(postres[]);
+}
+
+void arreglo(String arreglo[]){
+    
+ final copia = [];
+  
+  for(int i =0;arreglo.length;i++){
+  
+    copia[i] = arreglo [i];
+    
+    print(copia);
+    
+  }
+}
